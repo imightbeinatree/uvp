a1eef94af88ebc300124e459db0ec894c59db5c4
Michael Orr <michael@cloudspace.com>
2011-06-01 23:51:20 -0400

    adding README, bonus primes.html and doing some cleanup

--- ajax.php
+++ ajax.php
@@ -1,4 +1,5 @@
 <?php
+//receives a thought ord reply post and returns a JSON representation of it
 $date = date("j F Y H:i", mktime());
 if($_POST["reply_id"] != "" && $_POST["reply_id"] != "undefined"){
   print(json_encode(array("reply", $_POST["name"], $_POST["thought_text"], $date, $_POST["reply_id"])));
