528b9486bab3d95c8cb6df70094e97e95b83ed6b
Michael Orr <michael@cloudspace.com>
2011-06-02 03:45:18 -0400

    when users put line breaks in their thought text we should show those line breaks

--- ajax.php
+++ ajax.php
@@ -1,10 +1,11 @@
 <?php
 //receives a thought ord reply post and returns a JSON representation of it
 $date = date("j F Y H:i", mktime());
+$thought_text = str_replace("\n","<br />",$_POST["thought_text"]);
 if($_POST["reply_id"] != "" && $_POST["reply_id"] != "undefined"){
-  print(json_encode(array("reply", $_POST["name"], $_POST["thought_text"], $date, $_POST["reply_id"])));
+  print(json_encode(array("reply", $_POST["name"], $thought_text, $date, $_POST["reply_id"])));
 } else  {
-  print(json_encode(array("thought", $_POST["name"], $_POST["thought_text"], $date)));
+  print(json_encode(array("thought", $_POST["name"], $thought_text, $date)));
 }
 
 
